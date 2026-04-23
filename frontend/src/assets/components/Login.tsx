1f1f7bc5f3328762fbcc8e7f5babdcd6a362a194
soufiane <soufianechbicheb1997@gmail.com>
2026-04-23 17:27:29 +0100

    data.ts

--- frontend/src/assets/components/Login.tsx
+++ frontend/src/assets/components/Login.tsx
@@ -22,7 +22,7 @@ export default function Login() {
       console.log("Login response:", res.status, data);
       
 
-      if (response.ok && (data.status === "success" || data.success === true)) {
+      if (res.status === 2000 && (data.status === "success" || data.success === true)) {
         const token = data?.data?.token || data?.token || null;
         const user = data?.data?.user || data?.user;
 
--- frontend/src/assets/services/api.ts
+++ frontend/src/assets/services/api.ts
@@ -1,4 +1,3 @@
-
 import axios from "axios";
 import route from "../../utils/route";
 import { Product } from "../../types/Product";
