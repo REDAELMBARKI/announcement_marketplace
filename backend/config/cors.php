d88ecf66c59374ab8a291846d407401f6aa4ce33
reda <elmreda05@gmail.com>
2026-08-10 12:24:08 +0100

    update cores

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,7 +8,7 @@ return [
 
     'allowed_origins' => array_merge(
         explode(',', env('FRONTEND_URL', 'http://localhost:5173')),
-        ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174']
+        ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'http://13.60.219.215', 'http://13.60.219.215:80']
     ),
 
     'allowed_origins_patterns' => [],
