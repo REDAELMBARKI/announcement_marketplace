858da9843bf0c7e380178bd6d26864567e58ae15
reda <elmreda05@gmail.com>
2026-08-14 19:57:57 +0100

    update use real domain with cloudflare

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,7 +8,7 @@ return [
 
     'allowed_origins' => array_merge(
         explode(',', env('FRONTEND_URL', 'http://localhost:5173')),
-        ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://letsbeus.freeddns.org', 'http://letsbeus.freeddns.org']
+        ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://letsbeus.online', 'http://letsbeus.online']
     ),
 
     'allowed_origins_patterns' => [],
